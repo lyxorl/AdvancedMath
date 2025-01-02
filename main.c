53437982afa16dcafa3d1228e80c3bb687ab49b8
lyxorl <orlanmonjoly@gmail.com>
2025-01-02 16:16:58 +0100

    add fill zero and first test

--- main.c
+++ main.c
@@ -1,34 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct {
-    double** matrix;
+typedef struct {
+    double** grid;
     int sizeX;
     int sizeY;
-}typedef matrix;
+}matrix;
 
-matrix* createMatrix(int n,int m){ //No tested and add verification for n and m
-    matrix* result = malloc(sizeof(matrix));
-    result->sizeX = n;
-    result->sizeY = m;
-    result->matrix = malloc(sizeof(double)*m);
+matrix createMatrix(int n,int m){ 
+
+    if (n <= 0 || m <= 0){
+        printf("Enter positive intger\n");
+        exit(1);
+    }
+
+    matrix result;
+    result.sizeX = n;
+    result.sizeY = m;
+    result.grid = malloc(sizeof(double)*m);
     for (int i = 0; i<m; i++){
-        result->matrix[i] = malloc(sizeof(double)*n);
+        result.grid[i] = malloc(sizeof(double)*n);
     }
 
     return result;
 }
 
-matrix* transpose(matrix M){ //No tested and add verification to see if the matrix contain value
-    matrix* result = createMatrix(M.sizeY,M.sizeX);
-    for (int x; x<M.sizeX; x++){
-        for (int y; y<M.sizeY; y++){
-            result->matrix[y][x] = M.matrix[x][y];
+void zeroMatrix(matrix M){
+    //Fill the matrix with 0
+
+    for(int x = 0; x< M.sizeX; x++){
+        for(int y = 0; y< M.sizeY; y++){
+            M.grid[y][x] = 0.0;
+        }
+    }
+}
+
+matrix transpose(matrix M){ //No tested and add verification to see if the matrix contain value
+    matrix result = createMatrix(M.sizeY,M.sizeX);
+    for (int x = 0; x<M.sizeX; x++){
+        for (int y = 0; y<M.sizeY; y++){
+            result.grid[x][y] = M.grid[y][x];
         }
     }
     return result;
 }
 
+void printMatrix(matrix M){
+    // add parametter to format print with float
+    for (int x = 0; x<M.sizeX; x++){
+        for (int y = 0; y<M.sizeY; y++){
+            printf("%f ",M.grid[y][x]);
+        }
+        printf("\n");
+    }
+}
+
 int main(){
+    
+    /*---test---*/
+    /*
+    //Test error with matrix creation - OK
+    matrix matrixError = createMatrix(-1,5);
+    */
+    
+    /*
+    //Test fill 0 in matrix and transpose - OK
+    matrix matrixTest = createMatrix(5,7);
+    zeroMatrix(matrixTest);
+    matrixTest.grid[2][3] = 2.3;
+    matrix matrixTestTranspose = transpose(matrixTest);
+
+    printMatrix(matrixTest);
+    printf("\n");
+    printMatrix(matrixTestTranspose);
+    */
+    
     return 0;
 }
