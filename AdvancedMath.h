cd0e1c40278878c732ea22fe9da495958ca91685
lyxorl <orlanmonjoly@gmail.com>
2025-01-09 21:58:21 +0100

    remove function from .h

--- AdvancedMath.h
+++ AdvancedMath.h
@@ -32,18 +32,3 @@ typedef struct {
 }
 
 #endif
-
-matrix createMatrix(int n,int m);
-
-//fill_matrix.c
-matrix constantMatrix(int n, int m, double d);
-matrix nullMatrix(int n, int m);
-matrix scalarMatrix(int n, double d);
-matrix identityMatrix(int n);
-
-//classical_operator_matrix.c
-matrix product(matrix A, matrix B);
-matrix sum(matrix A, matrix B);
-
-//matrix_operator.c
-matrix transpose(matrix M);
