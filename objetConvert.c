90eb23c71cbf309e5efeff3cd5796b1245541baa
lyxorl <orlanmonjoly@gmail.com>
2025-01-11 20:35:26 +0100

    add function in convert

--- objetConvert.c
+++ objetConvert.c
@@ -10,3 +10,13 @@ Obj floatToObj(float d){
     return result;
 
 }
+
+float objToFloat(Obj o){
+
+    if (o.type != FLOAT){
+        printf("Error conversion : This Obj is not a float type");
+        exit(1);
+    }
+    return *(float *)(o.value);
+    
+}
